2c83a9d1de511aa911e9f94473ae511a18f8ec53
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2021-12-24 01:23:00 +0400

    started building register page && created corresponding styles file && installed => sass , prettier library

---
+++ pages/signin/Register.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Styles from "./Register.module.scss";
+const Register = () => {
+  return (
+    <div className={Styles.RegisterMainDiv}>
+      <div className={Styles.RegisterMainDivLeft}>marcxena</div>
+      <div className={Styles.RegisterMainDivRight}>marjvena</div>
+    </div>
+  );
+};
+export default Register;
