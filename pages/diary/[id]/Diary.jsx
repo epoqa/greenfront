8a7aea6e1578cbb37cc272bec2c9ad59dd525b7c
lashamarkhvaidze <lasha.marxvaidze@gmail.com>
2022-01-27 16:11:11 +0400

    Comments

--- pages/diary/[id]/Diary.jsx
+++ pages/diary/[id]/Diary.jsx
@@ -1,19 +1,28 @@
-import React, { useEffect, useRef, useState } from "react";
-
-//LIBRARY IMPORTS
-import axios from "axios";
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import ContentProvider from "../../../src/components/ContentProvider/ContentProvider";
+import Navigation from "../../../src/components/Navigation/Navigation";
+import Header from "../../../src/components/Header/Header";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import useValidateToken from "../../../src/reuseableFunctions/validateToken";
 import { useRouter } from "next/router";
-import { v4 as uuidv4 } from "uuid";
-
+import axios from "axios";
+import uniqid from "uniqid";
+import Loading from "../../../src/components/Loading/Loading";
 import { NotificationManager } from "../../../src/components/Notifications/Notifications";
-
-//MATERIAL UI IMPORTS
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import Image from "next/image";
+import { timeSince } from "../../../src/reuseableFunctions/timeSince";
 
-export default function Grower() {
+const mdTheme = createTheme();
+
+const CreateDiary = () => {
   const [diary, setDiary] = useState({});
   const [comments, setComments] = useState([]);
   const router = useRouter();
@@ -34,8 +43,8 @@ export default function Grower() {
   const commentRef = useRef();
 
   const sendRegisterInfoToBackend = (event) => {
+    console.log("asfasf");
     event.preventDefault();
-
     const commentRefValue =
       commentRef && commentRef.current && commentRef.current.value;
 
@@ -67,69 +76,130 @@ export default function Grower() {
   };
 
   return (
-    <>
-      <div
-        style={{ margin: "15px", padding: "20px", border: "1px solid black" }}
-      >
-        <h4>დღიურის სახელი: {diary.diaryName}</h4>
-        <h4>ტიპი: {diary.type}</h4>
-        <h4>აღწერა: {diary.description} </h4>
-        <h4>შემქმნელი: {diary.owner}</h4>
-        <h4>თარიღი: {diary.createdAt}</h4>
-        <h5>დღიურის აიდი: {diary._id}</h5>
-      </div>
-      <div
-        style={{ margin: "15px", padding: "20px", border: "1px solid black" }}
-      >
-        <h4>კომენტარები</h4>
-        <hr></hr>
-        <div>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  inputRef={commentRef}
-                  name="comment"
-                  required
-                  fullWidth
-                  id="comment"
-                  label="კომენტარი"
-                  autoFocus
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, bgcolor: "#3bbd2f" }}
-              onClick={(e) => sendRegisterInfoToBackend(e)}
-            >
-              დაწერა
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item></Grid>
-            </Grid>
-          </Box>
-        </div>
-        {comments
-          ? comments.map((comment) => (
-              <div
-                key={uuidv4()}
-                style={{
-                  margin: "5px",
-                  padding: "8px",
-                  border: "1px solid black",
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <Header />
+        <Navigation />
+        <ContentProvider>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={13} lg={13}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
                 }}
               >
-                <h3>{comment.owner}</h3>
-                <hr />
-                <h4>{comment.comment}</h4>
-                <h5>date: {comment.createdAt}</h5>
-              </div>
-            ))
-          : null}
-      </div>
-    </>
+                <div>
+                  <h4>დღიურის სახელი: {diary.diaryName}</h4>
+                  <h4>ტიპი: {diary.type}</h4>
+                  <h4>აღწერა: {diary.description} </h4>
+                  <h4>შემქმნელი: {diary.owner}</h4>
+                  <h4>თარიღი: {diary.createdAt}</h4>
+                  <h5>დღიურის აიდი: {diary._id}</h5>
+                </div>
+                <div>
+                  <div></div>
+                  <section style={{ backgroundColor: "#f7f6f6" }}>
+                    <div className="container my-5 py-5 text-dark">
+                      <div className="row d-flex justify-content-center">
+                        <div className="col-md-12 col-lg-10 col-xl-8">
+                          <div className="d-flex justify-content-between align-items-center mb-4">
+                            <h4 className="text-dark mb-0">
+                              კომენტარები ({comments.length})
+                            </h4>
+                            <br />
+                          </div>
+                          <form>
+                            <div className="row h-100 align-items-center">
+                              <textarea
+                                ref={commentRef}
+                                style={{ resize: "none" }}
+                                rows="2"
+                                cols="40"
+                                type="text"
+                                className="form-control my-2"
+                                id="diaryName"
+                                aria-describedby="diaryName"
+                                maxLength="700"
+                                required
+                              />
+
+                              <button
+                                type="submit"
+                                onClick={(e) => sendRegisterInfoToBackend(e)}
+                                className="col-sm-12 my-auto w-50 btn center btn-success"
+                              >
+                                დაწერა
+                              </button>
+                            </div>
+                          </form>
+                          <hr />
+                          <br />
+                          {comments
+                            ? comments.map((comment) => (
+                                <div key={uniqid()} className="card mb-3">
+                                  <div className="card-body">
+                                    <div className="d-flex flex-start">
+                                      <img
+                                        className="rounded-circle shadow-1-strong me-3"
+                                        src="https://avatars.githubusercontent.com/u/75354679?v=4"
+                                        alt="avatar"
+                                        width="40"
+                                        height="40"
+                                      />
+                                      <div className="w-100">
+                                        <div className="d-flex justify-content-between align-items-center mb-3">
+                                          <div className="mb-1 text-primary mb-0 text-lowercase">
+                                            <h6>{comment.owner}</h6>
+                                            <p className="text-dark small ms-2">
+                                              {comment.comment}
+                                            </p>
+                                          </div>
+                                        </div>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                          <p
+                                            className="small mb-0"
+                                            style={{ color: "#aaa" }}
+                                          >
+                                            <a href="#" className="link-grey">
+                                              წაშლა
+                                            </a>{" "}
+                                            •
+                                            <a href="#" className="link-grey">
+                                              {" რედაქტირება"}
+                                            </a>
+                                          </p>
+                                          <div className="d-flex flex-row">
+                                            <i className="fas fa-star text-warning me-2"></i>
+                                            <i
+                                              className="far fa-check-circle"
+                                              style={{ color: "#aaa" }}
+                                            ></i>
+                                            <p className="mb-0 small ">
+                                              {timeSince(comment.createdAt)} წინ
+                                            </p>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              ))
+                            : null}
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <br />
+              </Paper>
+            </Grid>
+          </Grid>
+        </ContentProvider>
+      </Box>
+    </ThemeProvider>
   );
-}
+};
+export default CreateDiary;
