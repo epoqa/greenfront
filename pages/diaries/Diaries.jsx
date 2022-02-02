81ed2584d79415a95d8c15a45908325e34b3b45d
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2022-02-02 22:48:41 +0400

    started refactoring Diaries.js page

--- pages/diaries/Diaries.jsx
+++ pages/diaries/Diaries.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable jsx-a11y/alt-text */
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
@@ -11,26 +9,25 @@ import Paper from "@mui/material/Paper";
 import styles from "../../styles/Home.module.css";
 import { useRouter } from "next/router";
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
-import uniqid from "uniqid";
 import axios from "axios";
+import SingleDiary from "../../src/components/SingleDIary/SingleDiary";
+import uniqid from "uniqid";
+
 const mdTheme = createTheme();
 
 const Home = () => {
   const router = useRouter();
   const [data, setData] = useState([]);
   useEffect(() => {
-      axios
-        .get(`https://greenbackk.herokuapp.com/diary/all`)
-        .then((res) => {
-          setData(res.data);
-         console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-  }, [router]);  
-
-  const arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    axios
+      .get(`https://greenbackk.herokuapp.com/diary/all`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [router]);
 
   return (
     <ThemeProvider theme={mdTheme}>
@@ -49,37 +46,16 @@ const Home = () => {
                   height: 500,
                 }}
               >
-                <h5 className='text-center my-1'>ყველა დღიური</h5>
-                <hr/>
-                <div className={`${styles.row} d-flex justify-content-center row`}>
-                      {data
-                        ? data.map((item, index) => (
-
-                            <div
-                              
-                              key={uniqid()}
-                              className={`${styles.column} mx-1 border border-secondary rounded column`}
-                              
-                            >
-                              <img
-                              style={{cursor: 'pointer'}}
-                              onClick={(e) => router.push(`/diary/${item.id}`)}
-                                className={`${styles.columnimg} rounded`}
-                                src={(item.weeks.length > 0) ? item.weeks.find(week => week.pictures[0] !== undefined).pictures[0].picture : 'https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg'}
-                                alt='სურათი'
-                              />
-                              <div>
-                                <p style={{cursor: 'pointer'}} onClick={(e) => router.push(`/diary/${item.id}`)} className='text-center my-1'>{item.diaryName}</p>
-                                <a href={'/grower/' + item.owner} className='text-center my-1'>{item.owner}</a>
-                                <br/>
-                                <small className='text-center'>{item.comments.length} კომენტარი • {item.weeks.length} კვირა</small>
-
-
-                              </div>
-                            </div>
-                          ))
-                        : null}
-                    </div>
+                <br />
+                <div
+                  className={`${styles.row} d-flex justify-content-around row`}
+                >
+                  {data
+                    ? data.map((item, index) => (
+                        <SingleDiary item={item} index={index} key={uniqid()} />
+                      ))
+                    : null}
+                </div>
               </Paper>
             </Grid>
           </Grid>
