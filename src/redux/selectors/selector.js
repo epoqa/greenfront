9733f8c444313fb769905952308831c4f9a1c234
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2022-02-03 14:47:14 +0400

    resolved bug then opening diary

--- src/redux/selectors/selector.js
+++ src/redux/selectors/selector.js
@@ -1,3 +1,3 @@
 export const getNavigationBar = (state) => state.navigationBar;
 export const getAddedImages = (state, chosenWeek) =>
-  state?.diary?.weeks ? state.diary.weeks[chosenWeek].pictures : null;
+  state?.diary?.weeks ? state.diary.weeks[chosenWeek]?.pictures : null;
